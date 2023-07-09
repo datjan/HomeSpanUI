45891a1c82d721798dacea2505e5647e0fe5cf17
Jan Kuepper <69371018+datjan@users.noreply.github.com>
2023-07-10 00:15:03 +0200

    Add files via upload

---
+++ src/HomeSpanUI/setup.h
@@ -0,0 +1,16 @@
+////////////////////////////////////////////////////////////
+// SETUP
+
+const char* wlan_ssid = "";   // Wifi SSID, the device will try to connect
+const char* wlan_pwd = "";    // Wifi PASSWORD, the device will try to connect
+
+// SETUP
+////////////////////////////////////////////////////////////
+// ADVANCED SETUP
+
+const char* ap_ssid = "HomeKitController";  // Access Point SSID
+const char* ap_pwd = "";               // Access Point PASSWORD (minimum 8 digits if used)
+const char* ota_pwd = "myotapass";  // Arduino OTA Password
+
+// ADVANCED SETUP
+////////////////////////////////////////////////////////////
